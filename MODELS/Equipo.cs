5feb9e9937cb7cb674225b0a6281fd1f316a12bd
willy <willy@WILLIAM-PC>
2026-07-02 21:24:47 -0500

    Agregar archivos de proyecto.

---
+++ MODELS/Equipo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace torneoPOO_GMERA.MODELS
+{
+    public class EQUIPO
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using torneoPOO_GMERA.MODELS;
+
+Jugador objJugador1 = new Jugador();
+
+objJugador1.Nombre = "Piero Hincapié";
+objJugador1.Numero = 3;
+objJugador1.Posicion = "Defensa";
+objJugador1.Edad = 25;
+
+objJugador1.Presentar();
+
+Jugador objJugador2 = new Jugador();
+
+objJugador2.Nombre = "Enner Valencia";
+objJugador2.Numero = 13;
+objJugador2.Posicion = "Delantero";
+objJugador2.Edad = 32;
+
+objJugador2.Presentar();
